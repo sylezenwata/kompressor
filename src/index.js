02b749d0a148e2920ee7ca9ad6cc345808a7ae7a
sylvester ezenwata <xthareal@gmail.com>
2021-09-14 05:28:30 +0100

    Updated returnBlob param to false  and optimzed convertToBlob func

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class Kompressor {
         callback = null,
         imageWidth = null,
         imageHeight = null,
-        returnBlob = true,
+        returnBlob = false,
         reduceQuality = true,
         validExtensionArray = ['jpg', 'jpeg', 'png']
     }) {
@@ -132,20 +132,19 @@ class Kompressor {
         try {
             // convert base64 to raw binary data held in a string
             // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-            var byteString = atob(dataURI.split(',')[1]);
+            const byteString = atob(dataURI.split(',')[1]);
             // separate out the mime component
-            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+            const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
             // write the bytes of the string to an ArrayBuffer
-            var ab = new ArrayBuffer(byteString.length);
+            let ab = new ArrayBuffer(byteString.length);
             // create a view into the buffer
             var ia = new Uint8Array(ab);
             // set the bytes of the buffer to the correct values
-            for (var i = 0; i < byteString.length; i++) {
+            for (let i = 0; i < byteString.length; i++) {
                 ia[i] = byteString.charCodeAt(i);
             }
             // write the ArrayBuffer to a blob, and you're done
-            var blob = new Blob([ab], {type: mimeString});
-            return blob;
+            return new Blob([ab], {type: mimeString});
         } catch (error) {
             // log error
             console.log(error);
